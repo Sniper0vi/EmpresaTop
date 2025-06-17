dcba091a23d1d03c8c3c025672843c27fc395fa0
unknown <sniper0bellini@gmail.com>
2025-06-17 13:37:46 -0300

    primeiro commit

--- empresa.js
+++ empresa.js
@@ -1,4 +1,15 @@
-let nome
-let endereço
-let distancia
-let entrega = [Urgente, Normal, urgente, normal]
+
+
+const promptSync = require("prompt-sync")();
+
+
+let nome;
+let endereço;
+let distancia;
+let entrega = {Urgente, Normal, urgente, normal};
+let nomeArquivo = "Informaçoes-do-Cliente.txt";
+
+do{
+    console.log("repita")
+    nome = promptSync("Qual o seu nome: ")
+}while (isNaN(nome))
