34eec99cf2000aa4238e5a3021ba3865b6be9a48
unknown <sniper0bellini@gmail.com>
2025-06-17 14:40:59 -0300

    primeiro commit

--- empresa.js
+++ empresa.js
@@ -5,25 +5,43 @@ const path = require("path");
 let nome;
 let endereço;
 let distancia;
-let entrega = ["urgente", "Urgente", "normal", "Normal"]
+let entrega;
+let urgente;
+let valorTotal;
 
 let nomeArquivo = "Informaçoes-do-Cliente.txt";
 
-do{
+do {
     nome = promptSync("Qual o seu nome: ")
-    if(!isNaN(nome)){
-        console.log("Seu nome é um numero? REPITA!!!") 
+    if (!isNaN(nome)) {
+        console.log("Seu nome é um numero? REPITA!!!");
     }
 
-    endereço = promptSync("Qual o numero da sua casa: ")
-    if(isNaN(endereço)){
-        console.log("Voce mora em um livro? REPITA!!!") 
+    endereço = promptSync("Qual o numero da sua casa: ");
+    if (isNaN(endereço)) {
+        console.log("Voce mora em um livro? REPITA!!!");
     }
 
-    distancia = promptSync("Qual a distancia da sua casa em Km: ")
-    if(isNaN(distancia)){
-        console.log("O seu portugues esta muito distante de voce. REPITA!!!") 
+    distancia = promptSync("Qual a distancia da sua casa em Km: ");
+    if (isNaN(distancia)) {
+        console.log("O seu portugues esta muito distante de voce. REPITA!!!");
     }
-    
+    urgente = promptSync("A sua compra é urgente? ")
+    if (!isNaN(urgente)) {
+        console.log("Sim ou nao, se decide")
+    }
 
-}while (!isNaN(nome) || isNaN(endereço) || isNaN(distancia))
+
+} while (!isNaN(nome) || isNaN(endereço) || isNaN(distancia) || !isNaN(urgente))
+
+
+
+
+if ((urgente == "sim")) {
+    valorTotal = (distancia * 1.5) * 1.2
+    // colocar aqui para mandar para o documento.txt
+} else {
+    valorTotal = (distancia * 1.5)
+    // colocar aqui para mandar para o documento.txt
+}
+console.log(valorTotal)
